a5b8c37e3d1ce107cf80e132b84d93acac89b9e6
Natink1 <natink4825@gmail.com>
2025-07-21 21:52:28 +0300

    Recieve message

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 import express from "express";
+import TelegramBot from "node-telegram-bot-api";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const token = "YOUR_TELEGRAM_BOT_TOKEN";
+const token = "6869606798:AAGgXJAskU9Taed7UFL8WXg_5D4RxfrfYQw";
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Basic route (Homepage)
-app.get("/", (req, res) => {
-  res.send("Welcome to My Blog!");
+// Start the server
+bot.on("message", (msg) => {
+  console.log(msg);
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, "Received your message");
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
