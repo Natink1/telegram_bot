b6ce726b45527d5c4d8aff459af948cfc2a2e7e9
Natink1 <natink4825@gmail.com>
2025-07-21 22:54:03 +0300

    OpenAI is not free anymore

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import express from "express";
 import TelegramBot from "node-telegram-bot-api";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+dotenv.config();
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -10,12 +17,17 @@ const token = "6869606798:AAGgXJAskU9Taed7UFL8WXg_5D4RxfrfYQw";
 const bot = new TelegramBot(token, { polling: true });
 
 // Start the server
-bot.on("message", (msg) => {
-  console.log(msg);
-  const chatId = msg.chat.id;
 
+bot.on("message", async (msg) => {
+  console.log(msg.text);
+
+  const response = await client.responses.create({
+    model: "gpt-4.1",
+    input: msg.text,
+  });
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, response.output_text);
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Received your message");
 });
 
 app.listen(PORT, () => {
